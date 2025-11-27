5a349a2b8b9cd31abc07c70c7f0f5bb30b5440ff
LCYLYM <ttymwan@gmail.com>
2025-11-28 01:32:52 +0800

    加入wap支持

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'loli-blog-v1';
+const URLS_TO_CACHE = ['/', '/index.html'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(URLS_TO_CACHE)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((names) =>
+      Promise.all(names.filter((n) => n !== CACHE_NAME).map((n) => caches.delete(n)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  const req = event.request;
+  if (req.method !== 'GET') return;
+  event.respondWith(
+    caches.match(req).then((res) => res || fetch(req).catch(() => caches.match('/index.html')))
+  );
+});
